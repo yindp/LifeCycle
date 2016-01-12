ab080b0698c0897c0feb3799b82cf21992b393fa
Simo YiN <378714297@qq.com>
2016-01-12 22:25:34 +0800

    Override activity lifecycle method.

--- app/src/main/java/com/example/colin/lifecycle/LifeCycleActivity.java
+++ app/src/main/java/com/example/colin/lifecycle/LifeCycleActivity.java
@@ -1,52 +1,113 @@
 package com.example.colin.lifecycle;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ActionMode;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class LifeCycleActivity extends AppCompatActivity {
+
+    private final String TAG = "LifeCycleActivity";
+    private Context context = this;
+    private int param = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lifecycle);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Log.i(TAG, "onCreate() called.");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_lifecycle);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(context, TargetActivity.class);
+                startActivity(intent);
             }
         });
     }
 
+    //Activity创建或者从后台重新回到前台时被调用
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart() called");
     }
 
+    //Activity创建或者从被覆盖、后台重新回到前台时被调用
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume() called.");
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    //Activity被覆盖到下面或者锁屏时被调用
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause() called.");
+    }
 
-        return super.onOptionsItemSelected(item);
+    //退出当前Activity或者跳转到新Activity时被调用
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop() called.");
+    }
+
+    //Activity从后台重新回到前台时被调用
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart() called");
+    }
+
+    //退出当前Activity时被调用,调用之后Activity就结束了
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestory() called");
+    }
+
+    //Activity窗口获得或失去焦点时被调用,在onResume之后或onPause之后
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        Log.i(TAG, "onWindowFocusChanged called.");
+    }
+
+    /**
+     * Activity被系统杀死时被调用.
+     * 例如:屏幕方向改变时,Activity被销毁再重建;当前Activity处于后台,系统资源紧张将其杀死.
+     * 另外,当跳转到其他Activity或者按Home键回到主屏时该方法也会被调用,系统是为了保存当前View组件的状态.
+     * 在onPause之前被调用.
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("param", param);
+        Log.i(TAG, "onSaveInstanceState called. put param: " + param);
+        super.onSaveInstanceState(outState);
+    }
+
+    /**
+     * Activity被系统杀死后再重建时被调用.
+     * 例如:屏幕方向改变时,Activity被销毁再重建;当前Activity处于后台,系统资源紧张将其杀死,用户又启动该Activity.
+     * 这两种情况下onRestoreInstanceState都会被调用,在onStart之后.
+     */
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        param = savedInstanceState.getInt("param");
+        Log.i(TAG, "onRestoreInstanceState called. get param: " + param);
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
