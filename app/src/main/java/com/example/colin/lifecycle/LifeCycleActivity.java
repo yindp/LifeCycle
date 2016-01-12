2150d5b1f3b8c71a1f0d100dcc0845c0774ba142
Simo YiN <378714297@qq.com>
2016-01-12 21:48:45 +0800

    Change the activity name(include the layout xml file.)

--- app/src/main/java/com/example/colin/lifecycle/LifeCycleActivity.java
+++ app/src/main/java/com/example/colin/lifecycle/LifeCycleActivity.java
@@ -9,12 +9,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class LifeCycleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lifecycle);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
